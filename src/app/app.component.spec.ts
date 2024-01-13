67ef5adb2c5dfcbdc98a176e657211b766de60ab
Andrzej Biernacki <andrzej.biernacki85@gmail.com>
2024-01-13 14:08:19 +0100

    Revert "Update app.component.spec.ts"
    This reverts commit 2f93a503aaf0a515ad59ff89a6de59f873b2d464.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'Angular-github-actions' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Broken unit test');
+    expect(app.title).toEqual('Angular-github-actions');
   });
 
   it('should render title', () => {
