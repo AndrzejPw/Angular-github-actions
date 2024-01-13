2f93a503aaf0a515ad59ff89a6de59f873b2d464
Andrzej Biernacki <andrzej.biernacki85@gmail.com>
2024-01-13 13:53:15 +0100

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'Angular-github-actions' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-github-actions');
+    expect(app.title).toEqual('Broken unit test');
   });
 
   it('should render title', () => {
